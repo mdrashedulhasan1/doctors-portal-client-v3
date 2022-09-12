1a5cf658635c7cba803ff7778226f84d6508d445
unknown <rashedul15-1718@diu.edu.bd>
2022-09-12 04:18:49 -0700

    Add SIGN_IN_WITH_GOOGLE

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Navbar from './components/Shared/Navbar';
 import Appointment from "./components/Appointment/Appointment";
+import Login from "./components/Login/Login";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home></Home>} />
         <Route path="/about" element={<About></About>} />
         <Route path="/appointment" element={<Appointment></Appointment>} />
+        <Route path="/login" element={<Login></Login>} />
       </Routes>
     </div>
   );
---
+++ src/components/Login/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import auth from '../../firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+
+const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if(user){
+        console.log(user);
+    }
+    return (
+        <div className='flex h-screen justify-center items-center'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center text-3xl font-bold">Login</h2>
+                    <div className="divider">OR</div>
+                    <button onClick={() => signInWithGoogle()} className="btn btn-outline btn-secondary">SIGNIN_WITH_GOOGLE</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
